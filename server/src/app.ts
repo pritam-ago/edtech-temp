b1054849f020732e8b3923984891497545a291bc
Vikas Pritam <87309275+pritam-ago@users.noreply.github.com>
2025-04-29 16:31:01 +0530

    added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,10 +5,18 @@ import courseRoutes from './routes/course.routes';
 import enrollmentRoutes from './routes/enrollment.routes';
 import progressRoutes from './routes/progress.routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
+
 const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
